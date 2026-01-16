0e429275513720032ce4c16f402eb7cf5b7100df
Yulxon <2053395074@qq.com>
2026-01-16 20:15:37 +0800

    add rime-ice for ibus

--- modules/gui.nix
+++ modules/gui.nix
@@ -31,7 +31,15 @@
     inputMethod = {
       enable = true;
       type = "ibus";
-      ibus.engines = with pkgs.ibus-engines; [ rime ];
+      ibus.engines = with pkgs.ibus-engines; [
+        (rime.override {
+          rimeDataPkgs = with pkgs; [
+            rime-ice
+            rime-moegirl
+            rime-zhwiki
+          ];
+        })
+      ];
     };
   };
 
